d33004a95e13f6b8115c60f8152796618630738d
Siddharth2207 <siddharthbhoite22@gmail.com>
2026-05-23 10:58:46 +0530

    fork tests

--- test/src/concrete/ERC4626Words.fork.t.sol
+++ test/src/concrete/ERC4626Words.fork.t.sol
@@ -2,20 +2,34 @@
 // SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
 pragma solidity =0.8.25;
 
-import {Test, console2} from "forge-std/Test.sol";
-import {FORK_RPC_URL_BASE, FORK_BLOCK_BASE} from "test/lib/LibFork.sol";
+import {Test} from "forge-std/Test.sol";
+import {
+    FORK_RPC_URL_BASE,
+    FORK_BLOCK_BASE,
+    WT_NVDA,
+    WT_AMZN,
+    WT_TSLA,
+    WT_MSTR,
+    WT_IAU,
+    WT_COIN,
+    WT_SPYM,
+    WT_SIVR,
+    WT_CRCL,
+    WT_BMNR,
+    WT_PPLT,
+    WT_QQQM,
+    WT_VWO,
+    WT_ARKK,
+    WT_SGOV
+} from "test/lib/LibFork.sol";
 import {LibOpERC4626ConvertToAssets} from "src/lib/op/erc4626/LibOpERC4626ConvertToAssets.sol";
 import {LibOpERC4626ConvertToShares} from "src/lib/op/erc4626/LibOpERC4626ConvertToShares.sol";
 import {OperandV2, StackItem} from "rain.interpreter.interface/interface/unstable/IInterpreterV4.sol";
 import {Float, LibDecimalFloat} from "rain.math.float/lib/LibDecimalFloat.sol";
 
-address constant VAULT_BASE = 0x78c31580c97101694C70022c83D570150c11e935;
-
 interface IERC4626Fork {
     function decimals() external view returns (uint8);
     function asset() external view returns (address);
-    function convertToAssets(uint256 shares) external view returns (uint256);
-    function convertToShares(uint256 assets) external view returns (uint256);
 }
 
 interface IERC20Fork {
@@ -23,70 +37,157 @@ interface IERC20Fork {
 }
 
 contract ERC4626WordsForkTest is Test {
-    IERC4626Fork internal vault;
-    uint8 internal shareDecimals;
-    uint8 internal assetDecimals;
-
     function setUp() external {
         vm.createSelectFork(FORK_RPC_URL_BASE, FORK_BLOCK_BASE);
-        vault = IERC4626Fork(VAULT_BASE);
-        shareDecimals = vault.decimals();
-        assetDecimals = IERC20Fork(vault.asset()).decimals();
     }
 
-    /// Verify convertToShares matches the vault's actual on-chain rate at the fork block.
-    /// At block 46360198: convertToShares(1e18 assets) = 99730647641058807 shares (≈0.997 per asset).
-    function testForkConvertToSharesMatchesVault() external view {
-        uint256 assetsRaw = 10 ** assetDecimals;
-        uint256 expectedSharesRaw = vault.convertToShares(assetsRaw);
-
-        console2.log("assetsRaw (input to vault)", assetsRaw);
-        console2.log("expectedSharesRaw (vault.convertToShares output)", expectedSharesRaw);
+    function assetsToShares(address vaultAddress, uint256 assetsRaw) internal view returns (uint256 sharesRaw) {
+        IERC4626Fork vault_ = IERC4626Fork(vaultAddress);
+        uint8 shareDecimals_ = vault_.decimals();
+        uint8 assetDecimals_ = IERC20Fork(vault_.asset()).decimals();
 
         StackItem[] memory inputs = new StackItem[](2);
         inputs[0] =
-            StackItem.wrap(Float.unwrap(LibDecimalFloat.packLossless(int256(uint256(uint160(VAULT_BASE))), 0)));
+            StackItem.wrap(Float.unwrap(LibDecimalFloat.packLossless(int256(uint256(uint160(vaultAddress))), 0)));
         inputs[1] =
-            StackItem.wrap(Float.unwrap(LibDecimalFloat.fromFixedDecimalLosslessPacked(assetsRaw, assetDecimals)));
+            StackItem.wrap(Float.unwrap(LibDecimalFloat.fromFixedDecimalLosslessPacked(assetsRaw, assetDecimals_)));
 
         StackItem[] memory outputs = LibOpERC4626ConvertToShares.run(OperandV2.wrap(0), inputs);
-
-        console2.log(
-            "actualShares (fixed decimal)",
-            LibDecimalFloat.toFixedDecimalLossless(Float.wrap(StackItem.unwrap(outputs[0])), shareDecimals)
-        );
-
-        Float expectedShares = LibDecimalFloat.fromFixedDecimalLosslessPacked(expectedSharesRaw, shareDecimals);
-        assertEq(
-            StackItem.unwrap(outputs[0]), Float.unwrap(expectedShares), "shares must match vault.convertToShares(1e18)"
-        );
+        sharesRaw = LibDecimalFloat.toFixedDecimalLossless(Float.wrap(StackItem.unwrap(outputs[0])), shareDecimals_);
     }
 
-    /// Verify convertToAssets matches the vault's actual on-chain rate at the fork block.
-    /// At block 46360198: convertToAssets(1e18 shares) = 100270062609660912 assets (≈1.003 per share).
-    function testForkConvertToAssetsMatchesVault() external view {
-        uint256 sharesRaw = 10 ** shareDecimals;
-        uint256 expectedAssetsRaw = vault.convertToAssets(sharesRaw);
-
-        console2.log("sharesRaw (input to vault)", sharesRaw);
-        console2.log("expectedAssetsRaw (vault.convertToAssets output)", expectedAssetsRaw);
+    function sharesToAssets(address vaultAddress, uint256 sharesRaw) internal view returns (uint256 assetsRaw) {
+        IERC4626Fork vault_ = IERC4626Fork(vaultAddress);
+        uint8 shareDecimals_ = vault_.decimals();
+        uint8 assetDecimals_ = IERC20Fork(vault_.asset()).decimals();
 
         StackItem[] memory inputs = new StackItem[](2);
         inputs[0] =
-            StackItem.wrap(Float.unwrap(LibDecimalFloat.packLossless(int256(uint256(uint160(VAULT_BASE))), 0)));
+            StackItem.wrap(Float.unwrap(LibDecimalFloat.packLossless(int256(uint256(uint160(vaultAddress))), 0)));
         inputs[1] =
-            StackItem.wrap(Float.unwrap(LibDecimalFloat.fromFixedDecimalLosslessPacked(sharesRaw, shareDecimals)));
+            StackItem.wrap(Float.unwrap(LibDecimalFloat.fromFixedDecimalLosslessPacked(sharesRaw, shareDecimals_)));
 
         StackItem[] memory outputs = LibOpERC4626ConvertToAssets.run(OperandV2.wrap(0), inputs);
+        assetsRaw = LibDecimalFloat.toFixedDecimalLossless(Float.wrap(StackItem.unwrap(outputs[0])), assetDecimals_);
+    }
 
-        console2.log(
-            "actualAssets (fixed decimal)",
-            LibDecimalFloat.toFixedDecimalLossless(Float.wrap(StackItem.unwrap(outputs[0])), assetDecimals)
-        );
+    function testNVDA() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_NVDA, 1e18);
+        assertEq(actualSharesRaw, 1e18);
 
-        Float expectedAssets = LibDecimalFloat.fromFixedDecimalLosslessPacked(expectedAssetsRaw, assetDecimals);
-        assertEq(
-            StackItem.unwrap(outputs[0]), Float.unwrap(expectedAssets), "assets must match vault.convertToAssets(1e18)"
-        );
+        uint256 actualAssetsRaw = sharesToAssets(WT_NVDA, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testAMZN() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_AMZN, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_AMZN, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testTSLA() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_TSLA, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_TSLA, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testMSTR() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_MSTR, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_MSTR, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testIAU() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_IAU, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_IAU, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testCOIN() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_COIN, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_COIN, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testSPYM() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_SPYM, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_SPYM, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testSIVR() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_SIVR, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_SIVR, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testCRCL() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_CRCL, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_CRCL, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testBMNR() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_BMNR, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_BMNR, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testPPLT() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_PPLT, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_PPLT, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testQQQM() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_QQQM, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_QQQM, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testVWO() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_VWO, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_VWO, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testARKK() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_ARKK, 1e18);
+        assertEq(actualSharesRaw, 1e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_ARKK, 1e18);
+        assertEq(actualAssetsRaw, 1e18);
+    }
+
+    function testSGOV() external view {
+        uint256 actualSharesRaw = assetsToShares(WT_SGOV, 1e18);
+        assertEq(actualSharesRaw, 0.997306647641058807e18);
+
+        uint256 actualAssetsRaw = sharesToAssets(WT_SGOV, 1e18);
+        assertEq(actualAssetsRaw, 1.002700626096609112e18);
     }
 }
